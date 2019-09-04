acc11d698b566f4d3260b0819a56626e5959714c
wujianbo <1063109138@qq.com>
2019-09-04 22:00:42 +0900

    hello world

---
+++ src/main/java/com/nick/baseconstruction/BaseconstructionApplication.java
@@ -0,0 +1,13 @@
+package com.nick.baseconstruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseconstructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseconstructionApplication.class, args);
+	}
+
+}
