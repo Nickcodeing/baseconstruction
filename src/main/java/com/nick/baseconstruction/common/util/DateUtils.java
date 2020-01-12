9a26a2c690b2b4cfe89992876ac8a20aa3e543e8
wujianbo <1063109138@qq.com>
2020-01-13 03:32:30 +0900

    application.properties 新增项目名 新增日期类（无用）

--- src/main/java/com/nick/baseconstruction/common/util/DateUtils.java
+++ src/main/java/com/nick/baseconstruction/common/util/DateUtils.java
@@ -1,6 +1,18 @@
 package com.nick.baseconstruction.common.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtils {
 
+    public static void method(){
 
+    }
+
+    public static String getCurrentTime(){
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String format = simpleDateFormat.format(date);
+        return format;
+    }
 }
---
+++ src/main/java/com/nick/baseconstruction/web/controller/DateController.java
@@ -0,0 +1,21 @@
+package com.nick.baseconstruction.web.controller;
+
+import com.nick.baseconstruction.common.util.DateUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/date")
+public class DateController {
+
+
+    @ResponseBody
+    @RequestMapping("/getCurrentTime")
+    public String method(){
+        String currentTime = DateUtils.getCurrentTime();
+        System.out.println(currentTime);
+        return currentTime;
+    }
+}
